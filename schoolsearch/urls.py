fdb253849f374ad37477d6c6135b9dc214a8a16f
SolarisXYZ <lifu.sun@outlool.com>
2020-09-13 02:51:40 +1200

    attempting to get a search functionality working.

--- schoolsearch/urls.py
+++ schoolsearch/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('/', include('googlesearch.urls')),
     path('polls/', include('polls.urls')),
 ]
