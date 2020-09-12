ae8d44030ce842a839864cedac5bcde66dca9230
SolarisXYZ <lifu.sun@outlool.com>
2020-09-13 03:31:34 +1200

    following new instruction to create input form.

--- schoolsearch/urls.py
+++ schoolsearch/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('/', include('googlesearch.urls')),
-    path('polls/', include('polls.urls')),
+    path('', include('search.urls')),
+    # path('polls/', include('polls.urls')),
 ]
